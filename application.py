c538ebfbe25780175ebd710c0de090dc5634d709
Alex Prosdocimo <85607036+ProsDIH@users.noreply.github.com>
2021-06-17 12:25:29 +0200

    Add files via upload

--- application.py
+++ application.py
@@ -7,6 +7,9 @@ from os import path
 
 import csv
 
+###################################################
+#Programmato da Alex Prosdocimo e Matteo Mirandola#
+###################################################
 
 application = Flask(__name__)
 
@@ -18,6 +21,10 @@ def index():
 
 @application.route("/getGraph", methods=["POST", "GET"])
 def getgraph():
+    #Metodo POST: responsabile di ottnere i dati in formato json dal server. 
+    #Il server si aspetta un campo data che contenga il nome di un file esistente nel server nella cartella /static/json/
+    #Se non trova il file da un 404
+    #Se non trova il campo data da un 400
     if request.method == "POST":
         if('data' in request.form):
             if(path.exists("static/jsons/" + request.form['data'] + ".json")):
@@ -30,6 +37,12 @@ def getgraph():
         else:
             return "<h1>400 BAD REQUEST"
     else:
+        #Metodo GET:
+        #si aspetta un campo graph che contenga uno dei nomi sotto presenti
+        #nel caso di mf e emig si aspetta anche un secondo campo che specifichi
+        #l'università o la provincia-
+        #Inoltre, iscrittiAtn e mf POSSONO (ma non devono necessariamente) avere
+        #un campo aggiuntivo che filtri i dati di uno specifico anno o per uno specifico sesso2
         if 'graph' in request.args:
 
             # HBar Graph per la paga oraria provinciale a seconda del livello di istruzione
@@ -68,17 +81,31 @@ def getgraph():
 
     return "<h1>400 BAD REQUEST"
 
-# Aggiornamento dati alla mezzanotte di ogni lunedi
+#Per aggiornare i dataset:
+#A causa di un errore nella creazione del file riguardante gli iscritti per ogni ateneo da parte del MIUR il file
+#riguardante gli iscritti per ateneo non sono scaricabili dinamicamente e va sostituito manualmente.
+#Allo stesso modo, i dati ottenuti tramite l'istat non sono scaricabili dinamicamente tramite la api in quanto
+#le sue prestazioni sono limitate (oltre a non permettere i filtri necessari per ottenere i file).
+#Il dataset delle provincie viene aggiornato automaticamente ogni settimana. Gli altri vanno sostituiti manualmente.
+
+#I dataset statici vanno inseriti nella cartella /static/notUpdating/
+#Il dataset riguardante gli iscritti per ateneo va scaricato a questo link http://dati.ustat.miur.it/dataset/3dd9ca7f-9cc9-4a1a-915c-e569b181dbd5/resource/32d26e28-a0b5-45f3-9152-6072164f3e63/download/iscrittixateneo.csv
+#e rinominato iscrittiAteneo.csv
+
+#Il dataset riguardante gli iscritti emigrati dalla regione è stato creato manualmente a partire da altri dati e non può essere aggiornato
+
+#I dataset riguardanti la percentuale di disoccupazione e la retribuzione oraria media sono reperibili a questo portale http://dati.istat.it/
+#Sfortunatamente la funzione di ricerca del sito è molto lenta e limitata, comunque sia i due data set sono "Tasso di Disoccupazione - Dati Provinciali"
+#e "Retribuzione oraria media per titolo di studio". In entrambi i casi, è necessario filtrare i risultati per le sole provincie del Veneto.
+#I file vanno rinominati retribuzioneMedia.csv e taxDisocc.csv
+
+#Fortunatamente, si aggiornano solo annualmente
+
 @application.route("/doUpdate")
 def updateData():
-    # File iscritti per ateneo
-    # Per qualche motivo, questo SPECIFICO file causa un errore 502 Bad Gateway.
-    # Più sotto ci sono le stesse identiche funzioni su un file diverso e funziona
-    # Quindi il file sarà statico
-    # iscritti = requests.get(
-    #    'http://dati.ustat.miur.it/dataset/3dd9ca7f-9cc9-4a1a-915c-e569b181dbd5/resource/32d26e28-a0b5-45f3-9152-6072164f3e63/download/iscrittixateneo.csv', allow_redirects=True)
-    #open('static/iscrittiAteneo.csv', 'wb').write(iscritti.content)
-    with open('static/notUpdating/iscrittiAteneo.csv', newline='') as f:
+    #File iscritti per ateneo
+    #I dati vengono inseriti in un dizionario come array, il formato è più sotto
+    with open('static/notUpdating/iscrittiAteneo.csv', newline='') as f: #Qui si può cambiare il nome del file se necessario, basta che sia in formato csv corretto
         reader = csv.reader(f)
         data = list(reader)[1:]
         iscrittiAteneo = {
@@ -104,7 +131,7 @@ def updateData():
              "wb").write(iscrittiAteneoJson.encode())
 
     # File iscritti emigrati in altre regioni
-    with open('static/notUpdating/iscrittiEmig.json', newline='') as f:
+    with open('static/notUpdating/iscrittiEmig.json', newline='') as f: #Qui si può cambiare il nome del file se necessario, basta che sia in formato csv corretto
 
         reader = json.load(f)
         iscrittiEmig = {
@@ -150,7 +177,7 @@ def updateData():
     # Formato: {"cittàInMinuscolo" : ["annoScolastico;CittàDiProvenienzaInMaiuscolo;RegioneDiEsodo;NumeroStudenti",...,...],...,...}
     open('static/jsons/iscrittiEmig.json',
          "wb").write(iscrittiEmigJson.encode())
-    # File reddito medio orario
+    # File paga media oraria per titolo di studio
     with open('static/notUpdating/retribuzioneMedia.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)[1:]
@@ -176,8 +203,8 @@ def updateData():
         open('static/jsons/retribuzioneMedia.json',
              "wb").write(retribuzioneMediaJson.encode())
 
-    # File disoccupazione
-    with open('static/notUpdating/taxDisocc.csv', newline='') as f:  # metti il file!!!!!!!!!!!!!!!1
+    # File %disoccupazione
+    with open('static/notUpdating/taxDisocc.csv', newline='') as f: #Qui si può cambiare il nome del file se necessario, basta che sia in formato csv corretto
         reader = csv.reader(f)
         data = list(reader)[1:]
         lavoro = {
@@ -192,7 +219,6 @@ def updateData():
         for row in data:
             if (row[7] == '15-24 anni') and row[5] != 'totale':
                 if row[5] == 'femmine':
-                    # Il dizionario è diviso in anno, tasso disoccupazione maschile, tasso disoccupazione femminile
                     lavoro[row[1]].append(str(row[10]))
                 else:
                     lavoro[row[1]].append(str(row[8]) + ';' + str(row[10]))
@@ -211,7 +237,7 @@ def updateData():
     # File iscritti totali per provincia
     iscritti = requests.get(
         'http://dati.ustat.miur.it/dataset/3dd9ca7f-9cc9-4a1a-915c-e569b181dbd5/resource/eae4ee94-0797-41d2-b007-bc6dad3ef3e2/download/iscrittixresidenza.csv', allow_redirects=True)
-    open('static/iscrittiProvincia.csv', 'wb').write(iscritti.content)
+    open('static/iscrittiProvincia.csv', 'wb').write(iscritti.content) #Qui si può cambiare il nome del file se necessario, basta che sia in formato csv corretto
     with open('static/iscrittiProvincia.csv', newline='') as f:
         reader = csv.reader(f)
         data = list(reader)[1:]
@@ -240,8 +266,7 @@ def updateData():
 #Startup#
 #########
 
-# Forcing Update of files on application startup
-
+#Ad ogni riavvio forzato dell'applicazione, i dati vengono aggiornati (ci impiega qualche secondo al maassimo)
 
 updateData()
 
